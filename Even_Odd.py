06002659db81fea33de4869c8e8d7f885d0f3beb
Shova Gelal <shovagelal33@gmail.com>
2025-09-16 10:05:17 +0545

    Program Executed Successfully

---
+++ Even_Odd.py
@@ -0,0 +1,7 @@
+#TO find even odd number take a number impinput from user and then using conditionals divide the number by two if the result is zero then its even otherwise its odd.
+user_input = int(input("Enter a Number: "))
+print("You have entered the number:", user_input)
+if user_input % 2 == 0:
+    print("The entered number is even")
+else:
+    print("The entered number is odd")
