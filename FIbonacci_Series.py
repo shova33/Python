9e05e3f009e09bac40c73b75ab91f72c641ae02c
Shova Gelal <shovagelal33@gmail.com>
2025-09-17 13:47:01 +0545

    Palindrome Number

--- FIbonacci_Series.py
+++ FIbonacci_Series.py
@@ -1,7 +1,7 @@
-limit = int(input("Enter the limit: "))
-fib_series = []
-a, b = 0, 1
-while a <= limit:
-	fib_series.append(a)
-	a, b = b, a + b
-print(fib_series)
+limit = int(input("Enter a number"))
+fibo_series = []
+a , b = 0 ,1
+while ( a <= limit) :
+    fibo_series.append(a)
+a , b = b, a+b
+print("The fibonacci series is :" , fibo_series)
---
+++ Palindrome_Number.py
@@ -0,0 +1,5 @@
+value = input("Enter a word or a number ;" )
+if (value == value[::-1]) :
+ print("The given number is palindrome")
+else:
+ print("The number is not palindrome")
